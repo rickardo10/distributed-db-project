b5ea6778cd597da8f4157f6ab3f9b79431055e17
Ricado Ocampo <r.ocampo.vega@gmail.com>
2014-11-21 12:40:57 +0000

    First Commit

---
+++ scripts/shard.sh
@@ -0,0 +1,18 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+sudo apt-get -y update
+
+sudo apt-get install -y vim
+
+sudo apt-get install -y mongodb-org
+
+sudo mkdir -p /data/db
+sudo chown -R vagrant /data/db
+
+sudo echo "10.0.0.11 shard01" >> /etc/hosts
+sudo echo "10.0.0.12 shard02" >> /etc/hosts
+sudo echo "10.0.0.13 shard03" >> /etc/hosts
+sudo echo "10.0.0.14 query01" >> /etc/hosts
+sudo echo "10.0.0.15 config01" >> /etc/hosts
